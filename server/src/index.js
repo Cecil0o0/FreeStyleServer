83aebc8302de1760e22f2e0bb615d4e3c71392d0
chenhaojie <jiegithub@gmail.com>
2017-12-09 21:11:02 +0800

    使用koa部署页面

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,9 @@ const app = new Koa()
 app.use(router.routes())
 app.use(router.allowedMethods())
 
+// static resources
+app.use(require('koa-static')(__dirname + '/dist'))
+
 // response
 app.use(async (ctx, next) => {
   next()

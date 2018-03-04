886cffa33e1849e0d90e3fa4cb45b376fc098439
chenhaojie <jiegithub@gmail.com>
2018-03-04 11:38:46 +0800

    feat: 增加斗罗大陆小说

--- server/src/util/splitNovel.js
+++ server/src/util/splitNovel.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
 const PathModule = require('path')
 const pinyin = require('pinyin')
+const nzhcn = require("nzh/cn")
 
 // 原小说文本分章节格式
 // 第001章 --- 第1200章
 const regex1 = /(\u7B2C\d+\u7AE0)/
+// 第一章 ....（三） --- 第二百三十六章 ...
+const regex2 = /(\u7B2C[\s\S]{1,5}\u7AE0)/
 
-const splitNovel = (pathToNovel = PathModule.resolve(__dirname, '../assets/novels/src/斗破苍穹.txt')) => {
+const splitNovel = (pathToNovel = PathModule.resolve(__dirname, '../assets/novels/src/斗罗大陆.txt')) => {
   return new Promise((resolve, reject) => {
     if (!pathToNovel) { reject('invalid path') }
     // 文件路径
@@ -23,6 +26,7 @@ const splitNovel = (pathToNovel = PathModule.resolve(__dirname, '../assets/novel
     const splitedDirname = PathModule.resolve(fileDirname, '../des', novelPinyin)
     // 创建分解文件目录
     if (fs.existsSync(splitedDirname)) {
+      rmdir(splitedDirname)
       splitChapter(filepath, splitedDirname)
     } else {
       mkdir(splitedDirname).then(() => {
@@ -38,7 +42,7 @@ const splitChapter = (srcNovelPath, desNovelPath) => {
   fs.readFile(srcNovelPath, {
     encoding: 'utf8'
   }, (err, data) => {
-    let arr = data.split(regex1)
+    let arr = data.split(regex2)
     arr.forEach((v, i) => {
       if (i === 0) {
         writeChapter(`${desNovelPath}/pre.txt`, arr[i]).then(() => {
@@ -47,7 +51,15 @@ const splitChapter = (srcNovelPath, desNovelPath) => {
       } else if (i % 2 === 1) {
         // 如果是奇数
         try {
-          const chapterNumber = Number(arr[i].match(/\d+/g))
+          let chapterNumber = 0
+          // 如果中间是数字，则直接使用
+          if (arr[i].match(/\d+/g)) {
+            chapterNumber = Number(arr[i].match(/\d+/))
+            // 否则转成数字
+          } else {
+            let str = arr[i].match(/\u7B2C([\s\S]{1,5})\u7AE0/)[1]
+            chapterNumber = Number(nzhcn.decodeS(str))
+          }
           const name = chapterNumber + '.txt'
           const content = `第${chapterNumber}章：${arr[i+1]}`
           writeChapter(`${desNovelPath}/${name}`, content).then(() => {
@@ -61,10 +73,10 @@ const splitChapter = (srcNovelPath, desNovelPath) => {
   })
 }
 
-const writeChapter = (path = '', data = '') => {
+const writeChapter = (path = '', data = '', options = { flag: 'a' }) => {
   return new Promise((resolve, reject) => {
     if (!data) data = ''
-    fs.writeFile(PathModule.resolve(path), data, (err, data) => {
+    fs.writeFile(PathModule.resolve(path), data, options, (err, data) => {
       if (err) reject(err)
       resolve()
     })
@@ -79,6 +91,14 @@ const mkdir = path => {
       } else {
         resolve()
       }
+    })
+  })
+}
+
+const rmdir = path => {
+  fs.readdir(path, (err, files) => {
+    files.forEach(filename => {
+      fs.unlinkSync(PathModule.resolve(path, filename))
     })
   })
 }
